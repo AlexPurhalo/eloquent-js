3c82e60794ceaf4f98f1abf9240f9d2cf4908288
AlexPurhalo <alexpurhalo@gmail.com>
2017-07-09 22:22:50 +0300

    looping-a-triangle

---
+++ program-structure/looping-a-triangle.js
@@ -0,0 +1,12 @@
+// // Do Loop example
+// var grid = ''
+// do { (grid += '#') && console.log(grid) } while (grid.length < 7);
+
+// // While loop example
+// var grid = '';
+// while (grid.length < 7) (grid += '#') && console.log(grid)
+
+
+
+// For loop example
+for (var grid = ''; grid.length <= 7; grid += '#') console.log(grid)
