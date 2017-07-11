ae500d17fe1003a764bccebee10c2859a70187a9
AlexPurhalo <alexpurhalo@gmail.com>
2017-07-12 01:16:28 +0300

    reversing-arr-with-recursion

--- data-structure/exercices/reversing-an-array.js
+++ data-structure/exercices/reversing-an-array.js
@@ -1,28 +1,21 @@
-const reverseArray = (arr, newArr, iterNum) => {
-	// const currI = iterNum || arr.length
-	// console.log(currI)
-	// const updatedArr = newArr || []
-	//
-	// if (currI === 0) return updatedArr
-	//
-	// updatedArr.unshift(arr[currI])
-	//
-	// return reverseArray(arr, updatedArr, currI - 1)
-}
-
 // A, B, C, D -> A, B, D, C -> A, D, B, C -> D, A, B, C      // 3
 // D, A, B, C -> D, A, C, B -> D, C, A, B ->                 // 2
 // D, C, A, B -> D, C, B, A                                  // 1
 
-const reverseArrayInPlace = (array, iterNum) => {
+const reverseArrayInPlace = (array, iterNum, initEndpoint) => {
 	if (iterNum === 0) return array
 	
 	const currIter = iterNum || array.length-1
+	const endpointI = array.length-1 - currIter
+	const currEndpoint = (initEndpoint >= 0 ? initEndpoint : array.length - 1)
 	
-	for (let i = array.length-1; i >= array.length - currIter; i--) {
-		const tempVal = array[i - 1]
-		array[i - 1] = array[i]
-		array[i] = tempVal
+	if (currEndpoint !== endpointI) {
+		const tempVal = array[currEndpoint - 1]
+		
+		array[currEndpoint - 1] = array[currEndpoint]
+		array[currEndpoint] = tempVal
+		
+		return reverseArrayInPlace(array, currIter, currEndpoint - 1)
 	}
 	
 	return reverseArrayInPlace(array, currIter - 1)
